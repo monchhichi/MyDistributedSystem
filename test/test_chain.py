62d19909e6fcaa29b082ba9688861e4fddce2fad
monchhichi <Yiyuan@Vanessa.local>
2016-04-25 13:08:53 -0400

    test added

--- test/test_chain.py
+++ test/test_chain.py
@@ -2,9 +2,12 @@ import httplib
 import time
 
 headers = {"Content-type": "application/json"}
-conn0 = httplib.HTTPConnection("localhost:7000")
-conn1 = httplib.HTTPConnection("localhost:8000")
-conn2 = httplib.HTTPConnection("localhost:9000")
+# conn0 = httplib.HTTPConnection("localhost:7000")
+# conn1 = httplib.HTTPConnection("localhost:8000")
+# conn2 = httplib.HTTPConnection("localhost:9000")
+conn0 = httplib.HTTPConnection("168.62.177.50:6666") # mine
+conn1 = httplib.HTTPConnection("40.117.95.162:6666") # yyc
+conn2 = httplib.HTTPConnection("104.42.186.101:6666") #charles
 
 conn0.request("POST", "/api/v1/add_node", '{ "node_id": 1 }', headers)
 conn0.getresponse().read()
