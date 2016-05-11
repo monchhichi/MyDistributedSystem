6426d629f67183fc178af74ba8dac6c415b8fb6f
Yiyuan Zhang <yiyuan.zhang@yale.edu>
2016-05-11 12:30:02 -0400

    command line argument

--- main.cpp
+++ main.cpp
@@ -45,16 +45,23 @@ static struct mg_serve_http_opts s_http_server_opts;
 
 const int DEBUG = 0;
 
-// RemoteServiceClient *client;
-
 static char s_http_port[20];
-static int rpc_port;
 
+int partitionIndex;
+vector<string> partitions;
+
+// local server
 char s_next_host[20];
 char s_next_http_port[20];
-int next_rpc_port;
 
-bool tailFlag = true;
+// partition servers
+string s_http_hosts[20];
+string s_http_ports[20];
+int partitionCount;
+
+int get_rpc_port(int partitionIdx) {
+    return partitionIdx + 20000;
+}
 
 void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
     struct http_message *hm = (struct http_message *) ev_data;
@@ -132,15 +139,15 @@ void handle_add_node(struct mg_connection *nc, struct http_message *hm) {
     else {
         printf("Unknown return value!\n");
     }
-    if (!tailFlag) {
-        boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
-        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-        RemoteServiceClient client(protocol);
-        transport->open();
-        client.rmt_add_node(id);
-        transport->close();
-    }
+    // if (!tailFlag) {
+    //     boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
+    //     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    //     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    //     RemoteServiceClient client(protocol);
+    //     transport->open();
+    //     client.rmt_add_node(id);
+    //     transport->close();
+    // }
 }
 
 void handle_remove_node(struct mg_connection *nc, struct http_message *hm) {
@@ -174,15 +181,15 @@ void handle_remove_node(struct mg_connection *nc, struct http_message *hm) {
         printf("Unknown return value!\n");
     }
 
-    if (!tailFlag) {
-        boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
-        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-        RemoteServiceClient client(protocol);
-        transport->open();
-        client.rmt_remove_node(id);
-        transport->close();
-    }
+    // if (!tailFlag) {
+    //     boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
+    //     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    //     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    //     RemoteServiceClient client(protocol);
+    //     transport->open();
+    //     client.rmt_remove_node(id);
+    //     transport->close();
+    // }
 }
 
 void handle_get_node(struct mg_connection *nc, struct http_message *hm) {
@@ -247,15 +254,15 @@ void handle_add_edge(struct mg_connection *nc, struct http_message *hm) {
     else {
         printf("Unknown return value!\n");
     }
-    if (!tailFlag) {
-        boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
-        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-        RemoteServiceClient client(protocol);
-        transport->open();
-        client.rmt_add_edge(a_id, b_id);
-        transport->close();
-    }
+    // if (!tailFlag) {
+    //     boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
+    //     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    //     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    //     RemoteServiceClient client(protocol);
+    //     transport->open();
+    //     client.rmt_add_edge(a_id, b_id);
+    //     transport->close();
+    // }
 }
 
 void handle_remove_edge(struct mg_connection *nc, struct http_message *hm) {
@@ -291,15 +298,15 @@ void handle_remove_edge(struct mg_connection *nc, struct http_message *hm) {
     else {
         printf("Unknown return value!\n");
     }
-    if (!tailFlag) {
-        boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
-        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-        RemoteServiceClient client(protocol);
-        transport->open();
-        client.rmt_remove_edge(a_id, b_id);
-        transport->close();
-    }
+    // if (!tailFlag) {
+    //     boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
+    //     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    //     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    //     RemoteServiceClient client(protocol);
+    //     transport->open();
+    //     client.rmt_remove_edge(a_id, b_id);
+    //     transport->close();
+    // }
 }
 
 void handle_get_edge(struct mg_connection *nc, struct http_message *hm) {
@@ -447,15 +454,15 @@ class RemoteServiceHandler : virtual public RemoteServiceIf {
   int32_t rmt_add_node(const int64_t node_id) {
     // Your implementation goes here
     int rc = add_node(node_id);
-    if (!tailFlag) {
-        boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
-        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-        RemoteServiceClient client(protocol);
-        transport->open();
-        client.rmt_add_node(node_id);
-        transport->close();
-    }
+    // if (!tailFlag) {
+    //     boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
+    //     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    //     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    //     RemoteServiceClient client(protocol);
+    //     transport->open();
+    //     client.rmt_add_node(node_id);
+    //     transport->close();
+    // }
     return rc;
     // printf("rmt_add_node\n");
   }
@@ -463,15 +470,15 @@ class RemoteServiceHandler : virtual public RemoteServiceIf {
   int32_t rmt_add_edge(const int64_t node_a_id, const int64_t node_b_id) {
     // Your implementation goes here
     int rc = add_edge(node_a_id, node_b_id);
-    if (!tailFlag) {
-        boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
-        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-        RemoteServiceClient client(protocol);
-        transport->open();
-        client.rmt_add_edge(node_a_id, node_b_id);
-        transport->close();
-    }
+    // if (!tailFlag) {
+    //     boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
+    //     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    //     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    //     RemoteServiceClient client(protocol);
+    //     transport->open();
+    //     client.rmt_add_edge(node_a_id, node_b_id);
+    //     transport->close();
+    // }
     return rc;
     // printf("rmt_add_edge\n");
   }
@@ -479,15 +486,15 @@ class RemoteServiceHandler : virtual public RemoteServiceIf {
   int32_t rmt_remove_node(const int64_t node_id) {
     // Your implementation goes here
     int rc = remove_node(node_id);
-    if (!tailFlag) {
-        boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
-        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-        RemoteServiceClient client(protocol);
-        transport->open();
-        client.rmt_remove_node(node_id);
-        transport->close();
-    }
+    // if (!tailFlag) {
+    //     boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
+    //     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    //     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    //     RemoteServiceClient client(protocol);
+    //     transport->open();
+    //     client.rmt_remove_node(node_id);
+    //     transport->close();
+    // }
     return rc;
     // printf("rmt_remove_node\n");
   }
@@ -495,15 +502,15 @@ class RemoteServiceHandler : virtual public RemoteServiceIf {
   int32_t rmt_remove_edge(const int64_t node_a_id, const int64_t node_b_id) {
     // Your implementation goes here
     int rc = remove_edge(node_a_id, node_b_id);
-    if (!tailFlag) {
-        boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
-        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-        RemoteServiceClient client(protocol);
-        transport->open();
-        client.rmt_remove_edge(node_a_id, node_b_id);
-        transport->close();
-    }
+    // if (!tailFlag) {
+    //     boost::shared_ptr<TTransport> socket(new TSocket(s_next_host, next_rpc_port));
+    //     boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    //     boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    //     RemoteServiceClient client(protocol);
+    //     transport->open();
+    //     client.rmt_remove_edge(node_a_id, node_b_id);
+    //     transport->close();
+    // }
     return rc;
     // printf("rmt_remove_edge\n");
   }
@@ -539,15 +546,14 @@ void runMongoose() {
 }
 
 void runThrift() {
-    // int port = 9000;
     boost::shared_ptr<RemoteServiceHandler> handler(new RemoteServiceHandler());
     boost::shared_ptr<TProcessor> processor(new RemoteServiceProcessor(handler));
-    boost::shared_ptr<TServerTransport> serverTransport(new TServerSocket(rpc_port));
+    boost::shared_ptr<TServerTransport> serverTransport(new TServerSocket(get_rpc_port(partitionIndex)));
     boost::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
     boost::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
     TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
-    printf("Starting thrift server on port %d\n", rpc_port);
+    printf("Starting thrift server on port %d\n", get_rpc_port(partitionIndex));
     server.serve();
 }
 
@@ -580,36 +586,48 @@ int main(int argc, char *argv[]) {
 
     extern char *optarg;
     int c;
-    while ((c = getopt(argc, argv, "a:b:")) != -1) {
+    // http server port
+    strcpy(s_http_port, argv[1]);
+    argv++;
+    while ((c = getopt(argc - 1, argv, "p:l:")) != -1) {
         if(optarg != NULL) {
             string s(optarg);
             vector<string> v = split(s, ' ');
+            int i = 0;
             switch (c) {
-              // localhost
-              case 'a':
-                strcpy(s_http_port, v[0].c_str());
-                rpc_port = atoi(v[1]);
-                break;
-              // next in chain
-              case 'b':
-                strcpy(s_next_host, v[0].c_str());
-                strcpy(s_next_http_port, v[1].c_str());
-                next_rpc_port = atoi(v[2]);
-                tailFlag = false;
-                break;
+                // localhost
+                case 'p':
+                    partitionIndex = atoi(v[0]);
+                    s_http_hosts[partitionIndex] = "localhost";
+                    s_http_ports[partitionIndex] = s_http_port;
+                    break;
+                // next in chain
+                case 'l':
+                    for (vector<string>::iterator it = v.begin() ; it != v.end(); ++it) {
+                        // cout << *it << endl;
+                        // skip self as servers are in order
+                        if (i == partitionIndex)
+                            i++;
+                        vector<string> tmp = split(*it, ':');
+                        s_http_hosts[i] = tmp[0];
+                        s_http_ports[i] = tmp[1];
+                        i ++;
+                    }
+                    partitionCount = i;
+                    break;
+                default:
+                    break;
             }
         }
         else {
             printf("optarg is null!!\n");
-        }
-        
+        }   
     }
 
-    cout << "This node: localhost...port(" << s_http_port << "), rpc port(" << rpc_port << ")" << endl;
-    if (tailFlag) {
-    cout << "Tail in chain." << endl;
-    } else{
-    cout << "Next node: " << s_next_host << "...port(" << s_next_http_port << "), rpc port(" << next_rpc_port << ")" << endl;
+    cout << "This node: localhost...port(" << s_http_port << "), rpc port(" << get_rpc_port(partitionIndex) << ")" << endl;
+
+    for (int i = 0; i < partitionCount; i ++) {
+        cout << "partition " << i << ":" << s_http_hosts[i] << "...port(" << s_http_ports[i] << "), rpc port(" << get_rpc_port(i) << ")" << endl;
     }
 
     std::thread mongooseThread(runMongoose);
